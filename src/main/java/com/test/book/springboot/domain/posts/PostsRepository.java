64c2050e70aba5bc934bfb2b38b67d928188b6f4
ok2qw66 <ok2qw66@gmail.com>
2021-05-04 19:32:47 +0900

    selectAll하여 전체리스트 불러오기

--- src/main/java/com/test/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/test/book/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,13 @@
 package com.test.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // mybatis의 Dao == JPA의 Repository extens JpaRepository<Entity 클래스, PK 타입>
 // Posts Entity 사용하려면 PostsRepository와 같은 위치에 있어야 한다
 public interface PostsRepository extends JpaRepository<Posts,Long> {
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/test/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/test/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.test.book.springboot.service.posts;
 
 import com.test.book.springboot.domain.posts.Posts;
 import com.test.book.springboot.domain.posts.PostsRepository;
+import com.test.book.springboot.web.dto.PostsListResponseDto;
 import com.test.book.springboot.web.dto.PostsResponseDto;
 import com.test.book.springboot.web.dto.PostsSaveRequestDto;
 import com.test.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final 필드 생성자를 알아서 lombok에서 생성 => 코드변경돼도 생성자코드수정 불필요
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //트랜잭션범위 유지 + 조회만 가능 => 속도 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream() //postsRepository.findAllDesc()의 결과를
+                .map(PostsListResponseDto::new) // map을 통해 PostsListResponseDto로 변환
+                .collect(Collectors.toList()); // List로 반환
     }
 }
