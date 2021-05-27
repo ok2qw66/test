61f8a24595ace46f0f471fe5d70a8f4ac0237d43
ok2qw66 <ok2qw66@gmail.com>
2021-05-27 20:22:22 +0900

    기존 test에 security 적용하기

--- src/test/java/com/test/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/test/book/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.test.book.springboot.web;
 
+import com.test.book.springboot.config.auth.SecurityConfig;
 import com.test.book.springboot.web.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
@@ -14,7 +18,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 스프링 부트 테스트와 JUnit 사이의 연결 역할 => JUit 내장 실행자 외의 다른 실행자 실행
 @RunWith(SpringRunner.class)
 // spring MVC에 집중 => controller,controllerAdvice 사용o / service,repository,componen 사용xa
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            })
 public class HelloControllerTest {
 
     // spring이 관리하는 bean 주입받는다 (DI)
@@ -22,7 +29,8 @@ public class HelloControllerTest {
     private MockMvc mvc; // 웹 API 테스트 시작점 => GET,POST 등의 API 테스트 가능
 
     @Test
-    public void callHello() throws Exception{
+    @WithMockUser(roles = "USER")
+    public void returnHello() throws Exception{
         String hello = "hello";
         mvc.perform(get("/hello")) // /hello 주수로 HTTP GET 요청
                 .andExpect(status().isOk()) // 응답이 200인지 체크
@@ -30,8 +38,9 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
 //callHelloDto  ===> 괄호안붙여서 에러...
-    public void callHelloDto() throws Exception{
+    public void returnHelloDto() throws Exception{
         String name = "hello";
         int amount = 1000;
 
