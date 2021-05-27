2ce40a6855cd49420a198dcb4ce1992091bf260f
ok2qw66 <ok2qw66@gmail.com>
2021-05-27 19:26:52 +0900

    네이버 로그인 api 사용하기

--- src/main/java/com/test/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/test/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,7 +25,20 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes); //Map 형태의 사용자 정보를 하나하나 반환하기
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder().name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
